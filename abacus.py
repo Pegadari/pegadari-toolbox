8db599915b9825004e721c94df4560790c3f4da7
Pegadari <71580795+Pegadari@users.noreply.github.com>
2022-12-03 00:52:13 +1000

    created super square-root
    + section comments
    + super_sqrt function

--- abacus.py
+++ abacus.py
@@ -3,7 +3,7 @@
 import math
 from scipy.special import lambertw
 
-
+# ---------- FACTORIAL ---------- #
 def inv_factorial(x):
     return int(inv_sterling_approx(x))
 
@@ -25,3 +25,16 @@ def inv_sterling_approx(x):
     # to find the lowest factorial base that goes into x, first try rounding the output, then rounding the other way
     # OR just round both ways and take the correct one
     # https://core.ac.uk/download/pdf/215383011.pdf
+
+
+# ---------- HYPEROPERATIONS ---------- #
+def super_sqrt(radicand: int) -> float:
+    """ Return the super square-root of the argument (reverse of 2nd tetration).
+        For an explanation, see https://en.wikipedia.org/wiki/Tetration#Square_super-root.
+        TODO:
+            - test which equation is faster
+    """
+
+    log_radicand = math.log(radicand)
+    return log_radicand / lambertw(log_radicand).real
+    return math.exp(lambertw(math.log(radicand)).real)
